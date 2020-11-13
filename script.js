620d0b5fcb5e9d46e816d385562eba333cd251ce
Olof-Wallgren <naturel.olof@hotmail.com>
2020-11-13 19:05:04 +0100

    klar

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 //start page, change years
 
-window.onload = startPage;
-function startPage(){
-    let years = diff_years(new Date, new Date(1987,2,6))
-   
+window.addEventListener('load',startPage); 
+function startPage() {
+    let years = diff_years(new Date, new Date(1987, 2, 6))
     document.getElementById('age').innerText = years;
-    startSlideshowAnimation();
+   addEventListeners();
+
 }
-function diff_years(dt2, dt1) 
-{
- let diff =(dt2.getTime() - dt1.getTime()) / 1000;
-  diff /= (60 * 60 * 24);
- return Math.abs(Math.round(diff/365.25))-1;
+function diff_years(dt2, dt1) {
+    let diff = (dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= (60 * 60 * 24);
+    return Math.abs(Math.round(diff / 365.25)) - 1;
 };
+
+function addEventListeners(){
+    document.getElementById('infoButton').addEventListener('click' , testLog);
+
+}
+
+function testLog(){
+    // let infoText = document.getElementById('textInfo')
+    document.getElementById("textInfo").classList.toggle("none-display");
+//    infoText.classList.remove('diplsay')
+//     // infoText.classList.add('display')
+//     console.log('ejrfernfe')
+}
